593d41f510f7c51f382249a21407c53ebf29ec92
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 06:06:37 +0000

    fixed building / user relationship

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,4 +2,5 @@ class Building < ActiveRecord::Base
   has_many :rooms
   has_many :containers, through: :rooms
   has_many :things, through: :containers
+  belongs_to :user
 end
--- db/migrate/20180307055329_create_buildings.rb
+++ db/migrate/20180307055329_create_buildings.rb
@@ -2,6 +2,7 @@ class CreateBuildings < ActiveRecord::Migration[5.1]
   def change
     create_table :buildings do |t|
       t.string :name
+      t.integer :user_id
     end
   end
 end
