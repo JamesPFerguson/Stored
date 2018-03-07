b47f45db5afb7ca6a236d1c00008f3fbd77dfa7a
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 12:08:52 +0000

    added app controller configuration and index route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 require './config/environment.rb'
 
 class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "thing_secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
 end
