9f778aca6bc2dadb3c54744a28ee849997ef65f7
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 05:47:09 +0000

    updated user model relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :buildings
+  has_many :rooms, through: :buildings
+  has_many :containers, through: :rooms
+  has_many :things, through: :containers
 end
