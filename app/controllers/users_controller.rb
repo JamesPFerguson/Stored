915e15a3aa255b2aec2d2146b2ae3bb08e1e01a7
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 16:21:23 +0000

    built routes for get and post signup with message for invalid values

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect '/users/show'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+   if params[:username] == "" || params[:password] == "" || params[:email] == ""
+     redirect '/signup'
+     flash[:message] = "Please enter valid informtion."
+   else
+     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+     @user.save
+     session[:user_id] = @user.id
+     redirect "/tweets"
+   end
+ end
+
+
+
 end
