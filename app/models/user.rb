b50163aee3e7dc6ebc84d70fef3de1e7368a9fba
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 17:38:43 +0000

    added css, fixed signup flash logic, added secure pass method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::KlassMethods
 
+  has_secure_password
   has_many :buildings
   has_many :rooms, through: :buildings
   has_many :containers, through: :rooms
