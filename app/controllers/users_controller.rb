c19f4ba95cec3abeeb5d3e21dd0c30a8acc0eb61
James Ferguson <ncjamesferguson@gmail.com>
2018-03-07 17:26:00 +0000

    cleaned up layout, added login routes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,35 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-   if params[:username] == "" || params[:password] == "" || params[:email] == ""
-     redirect '/signup'
-     flash[:message] = "Please enter valid informtion."
-   else
-     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-     @user.save
-     session[:user_id] = @user.id
-     redirect "/tweets"
-   end
- end
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect '/signup'
+      flash[:message] = "Please enter valid informtion."
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
 
- get '/login' do
-   erb :'/users/login'
- end
+  get '/login' do
+    if logged_in?
+      redirect "/users/show"
+    else
+      erb :'/users/login'
+    end
+  end
 
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/show"
+    else
+      flash[:message] = "Unable to Log in"
+      redirect "/login"
+    end
+  end
 
 
 end
