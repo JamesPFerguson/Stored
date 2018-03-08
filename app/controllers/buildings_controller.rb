02045aa9a54a2bdcc438e449da2fd52a0734c6ac
James Ferguson <ncjamesferguson@gmail.com>
2018-03-08 19:02:59 +0000

    added building routes

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,4 +1,39 @@
 class BuildingsController < ApplicationController
 
+  get '/buildings' do
+    s_user
+    erb :'/buildings/index'
+  end
+
+  get '/buildings/new' do
+    s_user
+
+
+    erb :'/buildings/new'
+  end
+
+  get '/buildings/:id' do
+    s_user
+    erb :'/buildings/show'
+  end
+
+  get '/buildings/:id/edit' do
+    s_user
+
+    erb :'/buildings.edit'
+  end
+
+  patch '/buildings/:id/' do
+    s_user
+
+    redirect "/buildings/#{@building.id}"
+  end
+
+  delete '/buildings:id' do
+    s_user
+
+    redirect "/buildings/index"
+  end
+
 
 end
