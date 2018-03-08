dd358c51be20060c3f8096da59e7f3d61a05e16c
James Ferguson <ncjamesferguson@gmail.com>
2018-03-08 19:51:37 +0000

    added building post route, error messages

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -6,6 +6,16 @@ class BuildingsController < ApplicationController
     erb :'/buildings/index'
   end
 
+  post '/buildings' do
+    if params[:building] != ""
+      building = Building.create(name: params[:building])
+      redirect "/buildings/#{building.id}"
+    else
+      flash[:message] = "Please enter a valid name"
+      redirect "/buildings/new"
+    end
+  end
+
   get '/buildings/new' do
     erb :'/buildings/new'
   end
@@ -22,20 +32,24 @@ class BuildingsController < ApplicationController
   end
 
   get '/buildings/:id/edit' do
-    s_user
-
-    erb :'/buildings/edit'
+    @building = Building.find(params[:id])
+    if @building.user == current_user
+      erb :'/buildings/edit'
+    else
+      redirect :"/error"
+    end
   end
 
   patch '/buildings/:id/' do
-    s_user
-
+    @building = Building.find(params[:id])
+    @building.update(name: params["building_name"])
+    @building.save
     redirect "/buildings/#{@building.id}"
   end
 
   delete '/buildings:id' do
-    s_user
-
+    @building = Building.find(params[:id])
+    @building.delete
     redirect "/buildings/index"
   end
 
