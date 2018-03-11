09104af6d7c33f43b10c159a29050039394b6889
James Ferguson <ncjamesferguson@gmail.com>
2018-03-11 00:44:05 +0000

    built show, index, and create routes

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -2,26 +2,43 @@ class ContainersController < ApplicationController
 
   get '/containers' do
     s_user
+    @containers = @user.containers
     erb :'/containers/index'
   end
 
   get '/containers/new' do
     s_user
-
-
+    @containers = @user.containers
     erb :'/containers/new'
   end
 
   post '/containers' do
-    s_user
-
-
-    redirect "/containers/#{@container.id}"
+    if params[:room].empty?
+      flash[:message] = "containers must belong to a room"
+      redirect "/containers/new"
+    elsif params[:container] != ""
+      room = room.find_by(name: params["room"])
+      container = container.find_or_create_by(name: params[:container])
+      if !room.containers.include?(container)
+        room.containers << container
+      end
+      redirect "/containers/#{container.id}"
+    else
+      flash[:message] = "Please enter a valid name"
+      redirect "/containers/new"
+    end
   end
 
   get '/containers/:id' do
-    s_user
-    erb :'/containers/show'
+    @container = Container.find(params[:id])
+    @building = @container.building
+    @room = @container.room
+    @things = Container.things
+    if current_user.containers.include?(@container)
+      erb :'/containers/show'
+    else
+      redirect :"/error"
+    end
   end
 
   get '/containers/:id/edit' do
@@ -33,7 +50,7 @@ class ContainersController < ApplicationController
   patch '/containers/:id/' do
     s_user
 
-    redirect "/containers/#{@building.id}"
+    redirect "/containers/#{@room.id}"
   end
 
   delete '/containers:id' do
